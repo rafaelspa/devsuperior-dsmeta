efa69f234655225caeef74e71bf59447ace05f8d
rafaelspa <rsdepaula67@gmail.com>
2022-11-20 15:32:59 -0300

    feat: date picker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import Header from './components/Header'
 import SalesCard from './components/SalesCard'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   return (
