4f9edfedc1c62301212b18a74d4400aff4faba43
rafaelspa <rsdepaula67@gmail.com>
2022-11-20 21:13:46 -0300

    feat: send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,24 @@
+import axios from "axios";
 import notificationIcon from "../../assets/img/icon.svg";
+import { BASE_URL } from "../../utils/request";
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number,
+}
+
+function handleClick(id : number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+           
+        })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <>
+        <div className="dsmeta-red-btn" onClick={() => { handleClick(saleId) }}>
             <img src={notificationIcon} alt="Notificar" />
-        </>
+        </div >
     )
 }
 
